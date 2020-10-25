3463d79daa2caa86e6f9665eb0a40cf51abd13bb
EC2 Default User <ec2-user@ip-172-31-69-152.ec2.internal>
2020-10-25 07:56:24 +0000

    finish 9

---
+++ Bookers2-9/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :messages, only: [:create, :destroy]
+  resources :rooms, only: [:create, :index, :show]
+end
